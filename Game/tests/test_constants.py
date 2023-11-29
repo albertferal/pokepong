8bc6d5261a71c19e30711cf4dc034dd8dac5830d
albertferal <albertferal@gmail.com>
2023-11-29 12:24:28 +0100

    first steps cicd

--- Game/tests/test_constants.py
+++ Game/tests/test_constants.py
@@ -1,9 +1,7 @@
 # Contenido de test_constants.py
 
-from constants import WIDTH, HEIGHT  # Importa las constantes que quieres probar desde constants.py
+from .. import constants  # Importa el módulo constants desde el directorio superior
 
 def test_constants_values():
-    assert WIDTH > 0  # Verifica si WIDTH es mayor que cero
-    assert HEIGHT > 0  # Verifica si HEIGHT es mayor que cero
-
-# Puedes agregar más pruebas para otras constantes o valores según sea necesario
+    assert constants.WIDTH > 0  
+    assert constants.HEIGHT > 0  
--- Game/tests/test_main.py
+++ Game/tests/test_main.py
@@ -1,8 +1,6 @@
 # Contenido de test_main.py
 
-from main import suma  # Importa la función que quieres probar desde main.py
+from .. import main  # Importa el módulo main desde el directorio superior
 
 def test_suma():
-    assert suma(2, 3) == 5  # Verifica si la suma de 2 y 3 es igual a 5
-
-# Puedes agregar más pruebas para diferentes casos o funciones según sea necesario
+    assert main.suma(2, 3) == 5  
