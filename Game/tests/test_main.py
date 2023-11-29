9aa9071d0c1c5916e552eda38ae8daa4b0bf1a08
albertferal <albertferal@gmail.com>
2023-11-29 13:25:02 +0100

    first steps cicd

--- Game/tests/test_main.py
+++ Game/tests/test_main.py
@@ -2,3 +2,19 @@ def test_pygame_initialization():
     import pygame
     pygame.init()
     assert pygame.display.get_init() == True
+
+
+def test_screen_creation():
+    import pygame
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    assert screen.get_width() == 800
+    assert screen.get_height() == 600
+
+
+def test_keyboard_events():
+    import pygame
+    pygame.init()
+    keys = pygame.key.get_pressed()
+    # Simula eventos de teclado y verifica si la tecla está presionada
+    assert keys[pygame.K_UP] == 0  # Por ejemplo, verificar si la tecla de flecha hacia arriba no está presionada inicialmente
