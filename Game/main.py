3831ca9d90719bc280c715b20ac4d2e8e87148cb
albertferal <albertferal@gmail.com>
2023-11-29 13:48:42 +0100

    pytest ok, testing lint

--- Game/main.py
+++ Game/main.py
@@ -4,7 +4,7 @@ import os
 # Inicializamos Pygame
 pygame.init()
 #pygame.mixer.init()  # Inicializa el módulo de sonido
-#goal_sound = pygame.mixer.Sound('assets\pokegol.mp3') 
+#goal_sound = pygame.mixer.Sound('assets\pokegol.mp3')
 #hit_sound = pygame.mixer.Sound("assets\pokehit.mp3")
 
 # Definimos algunas constantes
@@ -42,20 +42,19 @@ def handle_events():
             pygame.quit()
             sys.exit()
 
-ball_image = pygame.image.load(os.path.join("Game", "PKBALL.png")).convert_alpha()  # Reemplaza 'nombre_de_la_imagen.png' con tu nombre de archivo
-ball_image = pygame.transform.scale(ball_image, (BALL_RADIUS * 3, BALL_RADIUS * 3))  # Cambiar el tamaño de la imagen
+ball_image = pygame.image.load(os.path.join("Game", "PKBALL.png")).convert_alpha()
+ball_image = pygame.transform.scale(ball_image, (BALL_RADIUS * 3, BALL_RADIUS * 3))
 ball_image.set_colorkey((255, 255, 255))
 
 background_image = pygame.image.load(os.path.join("Game", "SCREENIMG2.png")).convert()
 ball_angle = 0  # Ángulo inicial de rotación
-
 
 J1_score = 0
 J2_score = 0
 
 # Bucle principal del juego
 while True:
-    
+
     handle_events()
     # Movimiento de las paletas
     keys = pygame.key.get_pressed()
@@ -67,7 +66,6 @@ while True:
         right_paddle_y -= 8
     if keys[pygame.K_DOWN] and right_paddle_y < HEIGHT - PADDLE_HEIGHT -10:
         right_paddle_y += 8
-
 
     # Movimiento de la pelota
     ball_x += ball_speed_x
@@ -88,7 +86,7 @@ while True:
     # Colisiones con las paredes superior e inferior
     if ball_y - BALL_RADIUS < 0 or ball_y + BALL_RADIUS > HEIGHT:
         ball_speed_y = -ball_speed_y
-        
+
     #Colisiones con las paredes izq y der
     if ball_x + 30 < 0:
         ball_x = WIDTH // 2
@@ -97,7 +95,7 @@ while True:
         ball_speed_x = 2
         ball_speed_y = 2
         J2_score +=1
-    
+
      #Colisiones con las paredes izq y der
     if ball_x - 30 > WIDTH:
         ball_x = WIDTH // 2
@@ -106,12 +104,11 @@ while True:
         ball_speed_x = -2
         ball_speed_y = -2
         J1_score +=1
-        
 
     # Dibujamos en la pantalla
     screen.fill(BLACK)
     screen.blit(background_image, (60, 200))
-    
+
     # Dibujar los marcadores en la pantalla
     font = pygame.font.Font(None, 36)
     left_score_text = font.render(f"RED: {J1_score}", True, WHITE)
@@ -124,15 +121,11 @@ while True:
     ball_rotated.set_colorkey((255,255,255))
     ball_rect = ball_rotated.get_rect(center=(ball_x, ball_y))
 
-    # Actualizar el ángulo para la próxima iteración (ajusta la velocidad de rotación según tu preferencia)
     ball_angle += 10  # Puedes cambiar el valor para ajustar la velocidad de giro
-    # Dibujar la imagen rotada en la pantalla
     screen.blit(ball_rotated, ball_rect)
 
-    #pygame.draw.circle(screen, WHITE, (ball_x, ball_y), BALL_RADIUS)
     pygame.draw.rect(screen, RED, (PADDLE_WIDTH, left_paddle_y, PADDLE_WIDTH, PADDLE_HEIGHT))
     pygame.draw.rect(screen, BLUE, (WIDTH - 2 * PADDLE_WIDTH, right_paddle_y, PADDLE_WIDTH, PADDLE_HEIGHT))
-    
 
     # Actualizamos la pantalla
     pygame.display.flip()
